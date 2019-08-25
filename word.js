9c448fc644dd04c69ea6ea717223682d47e9b36b
Ryan Mobley <ryan.mobley94@gmail.com>
2019-08-25 15:18:34 -0500

    building word obj

---
+++ word.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+class word{
+    constructor(n){
+        this.fullWord = n;
+        this.hiddenWord = this.Underscores();
+    }
+    Underscores(){
+        let str = [];
+        for(let i = 0; i < this.fullWord.length; i++){
+            str.push('_');
+        }
+        return str;
+    }
+    guess(letter){
+        if()
+    }
+}
+function writeWords(){
+    let arr = ["punch out","skyrim","team fortress","mario","doom"];
+    fs.writeFile('list.txt',arr,(err)=>{
+        if(err){
+            console.log("FAILED TO WRITE/n"+err);
+        }
+    });
+}
+function readWords(){
+    fs.readFile('list.txt','utf-8',(err,data)=>{
+        if(err){
+            console.log('what have you done?/n'+err);
+        }
+        let readArr = data.split(',');
+        return readArr;
+    });
+}
+function main(){
+    let triviaList = readWords();
+
+}
+main();
